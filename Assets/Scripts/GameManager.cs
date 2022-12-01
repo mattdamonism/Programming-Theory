d54b46cd7d779710451bfca97a7442a11b2ae4ec
mdomer@ipitomy.com <55860069+mattdamonism@users.noreply.github.com>
2022-11-30 23:21:25 -0500

    JWT token from react

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
+    public TMP_Text text;
+
     public string PlayerName;
+
+    public User user;
 
     private void Awake()
     {
@@ -17,5 +23,25 @@ public class GameManager : MonoBehaviour
         } 
         Instance = this;
         DontDestroyOnLoad(Instance);
+    }
+
+    public void SetZooInfo(string jwt)
+    {
+        user = JsonUtility.FromJson<User>(jwt);
+        text.text = user.username;
+    }
+
+    [Serializable]
+    public class User
+    {
+        public string username;
+        public string primaryAddress;
+        public string mirrorAddress;
+        public string mirrorBalance;
+        public string chainId;
+        public string verify2FASuccessTimes;
+        public string createTime;
+        public string iat;
+        public string allowance;
     }
 }
